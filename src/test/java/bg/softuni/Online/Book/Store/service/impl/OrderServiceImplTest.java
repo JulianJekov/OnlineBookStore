67887dfaa8205695e775215ac342b625248bee0c
Julian Jekov <drmsz@abv.bg>
2024-07-22 18:03:27 +0300

    Review Service Test

---
+++ src/test/java/bg/softuni/Online/Book/Store/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,7 @@
+package bg.softuni.Online.Book.Store.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+}
--- src/test/java/bg/softuni/Online/Book/Store/service/impl/ReviewServiceImplTest.java
+++ src/test/java/bg/softuni/Online/Book/Store/service/impl/ReviewServiceImplTest.java
@@ -2,7 +2,8 @@ package bg.softuni.Online.Book.Store.service.impl;
 
 import bg.softuni.Online.Book.Store.exceptions.ObjectNotFoundException;
 import bg.softuni.Online.Book.Store.model.dto.review.ReviewDTO;
-import bg.softuni.Online.Book.Store.model.entity.BookStoreUserDetails;
+import bg.softuni.Online.Book.Store.model.entity.Book;
+import bg.softuni.Online.Book.Store.model.entity.Review;
 import bg.softuni.Online.Book.Store.model.entity.User;
 import bg.softuni.Online.Book.Store.repository.BookRepository;
 import bg.softuni.Online.Book.Store.repository.ReviewRepository;
@@ -15,11 +16,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -49,7 +50,7 @@ class ReviewServiceImplTest {
     }
 
     @Test
-    void createReviewUserNotFoundShouldThrowException() {
+    void testCreateReviewUserNotFoundShouldThrowException() {
         ReviewDTO reviewDTO = new ReviewDTO();
         Long id = 1L;
 
@@ -57,6 +58,71 @@ class ReviewServiceImplTest {
 
         assertThrows(ObjectNotFoundException.class, () -> reviewServiceImpl.createReview(reviewDTO, id));
         verify(userRepository).findById(id);
+    }
+
+    @Test
+    void testCreateReviewBookNotFoundShouldThrowException() {
+        Long id = 1L;
+        ReviewDTO reviewDTO = new ReviewDTO();
+        reviewDTO.setId(id);
+        Long bookId = reviewDTO.getBookId();
+
+        User user = new User();
+        user.setId(id);
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        when(bookRepository.findById(bookId)).thenReturn(Optional.empty());
+
+        assertThrows(ObjectNotFoundException.class, () -> reviewServiceImpl.createReview(reviewDTO, id));
+        verify(userRepository).findById(id);
+        verify(bookRepository).findById(bookId);
+    }
+
+    @Test
+    void testCreateReviewShouldCreateReview() {
+        Long id = 1L;
+        ReviewDTO reviewDTO = createReviewDTO();
+        reviewDTO.setBookId(id);
+
+        User user = new User();
+        user.setId(id);
+
+        Book book = new Book();
+        book.setId(reviewDTO.getBookId());
+
+        Review review = new Review();
+        review.setId(id);
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        when(bookRepository.findById(reviewDTO.getBookId())).thenReturn(Optional.of(book));
+        when(modelMapper.map(reviewDTO, Review.class)).thenReturn(review);
+        reviewServiceImpl.createReview(reviewDTO, id);
+
+        verify(userRepository).findById(id);
+        verify(bookRepository).findById(reviewDTO.getBookId());
+        verify(modelMapper).map(reviewDTO, Review.class);
+        verify(reviewRepository).save(review);
+
+        assertEquals(user, review.getUser());
+        assertEquals(book, review.getBook());
+    }
+
+    @Test
+    void testDeleteShouldDeleteReview() {
+        Long id = 1L;
+
+        reviewServiceImpl.delete(id);
+
+        verify(reviewRepository).deleteById(id);
+    }
+
+    private static ReviewDTO createReviewDTO() {
+        return new ReviewDTO()
+                .setId(1L)
+                .setBookId(1L)
+                .setCreated(LocalDate.now())
+                .setComment("test")
+                .setRating(1);
     }
 
 }
